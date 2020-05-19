2977261cfda30db34029408d59252c0264cf66ec
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-05-19 18:40:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
+<<<<<<< HEAD
 var conversation = `Let\'s talk about ${topic}.`;
+=======
+var conversation = `${topic}`;
+>>>>>>> 22faa778031e7f6c5f0b837df6041e29694ed08e
 
